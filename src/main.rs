c574225a9fab075edc2a37692aa105f8c3ff6a43
TNAucoin <travisdevaucoin@gmail.com>
2024-03-22 15:53:35 -0500

    extract ebs function out of cli invoke

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,9 @@ async fn main() -> Result<()> {
     match matches.subcommand() {
         Some(("ec2", sub_m)) => match sub_m.subcommand() {
             Some(("get-available-ebs-volumes", args)) => {
-                let local_config = aws_config::load_defaults(BehaviorVersion::latest()).await;
                 let role: &String = args.get_one("role").unwrap();
                 let account: &String = args.get_one("account").unwrap();
-                let config = aws_core::get_config(role, account, &local_config).await?;
-                let available_volumes = ec2::get_available_ebs_volumes(config).await?;
-                println!("{:?}", available_volumes);
+                get_available_ebs_volumes(role,account).await?;
             }
             _ => {
                 // No valid subcommand was found, this is handled by clap.
@@ -27,5 +24,13 @@ async fn main() -> Result<()> {
         },
         _ => {}
     }
+    Ok(())
+}
+
+async fn get_available_ebs_volumes(role: &String, account: &String) -> Result<()> {
+    let local_config = aws_config::load_defaults(BehaviorVersion::latest()).await;
+    let config = aws_core::get_config(role, account, &local_config).await?;
+    let available_volumes = ec2::get_available_ebs_volumes(config).await?;
+    println!("{:?}", available_volumes);
     Ok(())
 }
